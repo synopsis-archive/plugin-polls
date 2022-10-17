743c0c323bd244e7a47afe6d054ecc243efc513b
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2022-10-17 09:17:19 +0200

    used ansichten

--- src/app/feature-eager/feature-eager-routing.module.ts
+++ src/app/feature-eager/feature-eager-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LehreransichtComponent } from './lehreransicht/lehreransicht.component';
+import { SchueleransichtComponent } from './schueleransicht/schueleransicht.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'umfragen',
+  children: [
+    { path: 'Lehreransicht', component: LehreransichtComponent },
+    { path: 'Schueleransicht', component: SchueleransichtComponent }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LehreransichtComponent } from './lehreransicht/lehreransicht.component';
-import { SchueleransichtComponent } from './schueleransicht/schueleransicht.component';
-
 
 
 @NgModule({
   declarations: [
-    LehreransichtComponent,
-    SchueleransichtComponent
   ],
   imports: [
     CommonModule
