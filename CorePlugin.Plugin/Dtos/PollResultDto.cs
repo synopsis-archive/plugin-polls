d73d945aa2a4d5b4eb43d0f5e70161c69e0a9863
EinboeckFranz <franz.einboeck.htlmail@gmail.com>
2022-11-17 13:58:01 +0100

    add documentation for results map

--- CorePlugin.Plugin/Dtos/PollResultDto.cs
+++ CorePlugin.Plugin/Dtos/PollResultDto.cs
@@ -5,5 +5,10 @@ namespace CorePlugin.Plugin.Dtos;
 public class PollResultDto : PollDto
 {
     [Required] public int ReceivedAnswers { get; set; }
+
+    /// <summary>
+    /// Key is the PollOptionId
+    /// Value is an object which contains the number of votes and the percentage which is calculated by the number of votes and the total number of votes
+    /// </summary>
     [Required] public Dictionary<long, ReceivedVotesDto> Results { get; set; } = null!;
 }
