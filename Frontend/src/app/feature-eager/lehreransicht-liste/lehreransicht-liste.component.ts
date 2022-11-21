875a2177d03cbe2f1446c187b4e270d28a869f51
Sarah <mayr.sarah.2003@gmail.com>
2022-11-21 09:36:49 +0100

    Created "lehreransicht-liste"-Component

---
+++ Frontend/src/app/feature-eager/lehreransicht-liste/lehreransicht-liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lehreransicht-liste',
+  templateUrl: './lehreransicht-liste.component.html',
+  styleUrls: ['./lehreransicht-liste.component.scss']
+})
+export class LehreransichtListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
