1af6dff283d3b3078606e950462ceef6bdbc1b68
fnadler18 <florian.nadler1@gmail.com>
2022-11-21 08:38:52 +0100

    fixed error

--- Frontend/src/app/feature-eager/feature-eager.module.ts
+++ Frontend/src/app/feature-eager/feature-eager.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { FeatureEagerRoutingModule } from './feature-eager-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { ErgebnissAnsichtComponent } from './ergebniss-ansicht/ergebniss-ansicht.component';
 
 
 @NgModule({
   declarations: [
-    ErgebnissAnsichtComponent
+    
   ],
   imports: [
     CommonModule,
