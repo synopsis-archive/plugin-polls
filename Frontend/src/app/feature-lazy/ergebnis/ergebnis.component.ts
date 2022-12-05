69febf8a1bdaa267dcacc9f511e9a77acb4254d0
fnadler18 <florian.nadler1@gmail.com>
2022-12-05 08:26:35 +0100

    Ergebnisansicht Working

--- Frontend/src/app/feature-lazy/ergebnis/ergebnis.component.ts
+++ Frontend/src/app/feature-lazy/ergebnis/ergebnis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataset, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-ergebnis',
@@ -11,6 +12,39 @@ export class ErgebnisComponent implements OnInit {
   umfragenFrage = "Was essen Wombats?";
   umfragenErsteller = "Florian Nadler";
   umfragenDatum = "21.11.2022";
+  umfragenCode = "2LM9X8"
+
+  chartData: ChartDataset[] = [{
+    label: '$ in millions',
+    data: [3, 4, 2, 6, 8]
+  }];
+  chartLabels: string[] = ['Bambus', 'Alex', 'Laub', 'Eukalyptus', 'wos ondas'];
+  chartOptions: ChartOptions = {
+
+    // ⤵️ Fill the wrapper
+    responsive: true,
+    maintainAspectRatio: false,
+
+    // ⤵️ Remove the grids
+    scales: {
+      xAxis: {
+        display: false,
+        grid: {
+          drawBorder: false // removes random border at bottom
+        }
+      },
+      yAxis: {
+        display: false
+      }
+    },
+
+    // ⤵️ Remove the main legend
+    plugins: {
+      legend: {
+        display: true
+      }
+    }
+  };
 
   constructor() { }
 
