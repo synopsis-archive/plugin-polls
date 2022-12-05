7e53e0680a0315ad21e08c803269763ead66375b
AlexanderSchallmeiner <raidyyou@gmail.com>
2022-12-05 08:44:00 +0100

    Fixed Error

--- Frontend/src/app/feature-lazy/ergebnis/ergebnis.component.ts
+++ Frontend/src/app/feature-lazy/ergebnis/ergebnis.component.ts
@@ -30,7 +30,7 @@ export class ErgebnisComponent implements OnInit {
       xAxis: {
         display: false,
         grid: {
-          drawBorder: false // removes random border at bottom
+         // removes random border at bottom
         }
       },
       yAxis: {
