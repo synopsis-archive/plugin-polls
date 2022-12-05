9f0d7f31fc2515eb06a9f2c96ab109885d5acd1e
Jan Schwendtner <SchwendtnerJ170070@sus.htl-grieskirchen.at>
2022-12-05 08:57:11 +0100

    PageNotFound created

--- Frontend/src/app/feature-lazy/feature-lazy.module.ts
+++ Frontend/src/app/feature-lazy/feature-lazy.module.ts
@@ -5,12 +5,14 @@ import { FeatureLazyRoutingModule } from './feature-lazy-routing.module';
 import { SchueleransichtComponent } from './schueleransicht/schueleransicht.component';
 import { ErgebnisComponent } from './ergebnis/ergebnis.component';
 import { NgChartsModule } from 'ng2-charts';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
     SchueleransichtComponent,
-    ErgebnisComponent
+    ErgebnisComponent,
+    PageNotFoundComponent
   ],
   imports: [
     CommonModule,
