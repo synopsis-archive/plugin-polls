09bfa98fd52eeb6f5fa547c60ad056da9ed390d8
Tobias Wimmer <tobiwimma04@gmail.com>
2022-12-19 09:39:00 +0100

    use backend code

--- frontend/apps/frontend/src/app/feature-eager/code-input/code-input.component.ts
+++ frontend/apps/frontend/src/app/feature-eager/code-input/code-input.component.ts
@@ -1,31 +1,39 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { PollsService } from '../../swagger';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-code-input',
   templateUrl: './code-input.component.html',
   styleUrls: ['./code-input.component.scss']
 })
-export class CodeInputComponent  {
+export class CodeInputComponent {
 
   code: string = "";
   errorHidden = true;
 
-  constructor(private router:Router) { }
+  constructor(private router: Router, private service: PollsService) { }
 
-  
 
-  enterPressend():void{
-    if(this.code.length === 6){
+
+  enterPressend(): void {
+    if (this.code.length === 6) {
       console.log(this.code);
-      this.router.navigate(['/Schueleransicht/'+this.code]);
+      this.service.pollsGetPollPollCodeGet(this.code)
+        .pipe(catchError((nadler, x) => {
+          this.errorHidden = false;
+          setTimeout(() => this.hideError(), 3000);
+          return nadler;
+        }))
+        .subscribe(poll => this.router.navigate(['/Schueleransicht/' + this.code]));
     } else {
       this.errorHidden = false;
       setTimeout(() => this.hideError(), 3000);
     }
   }
 
-  hideError():void{
+  hideError(): void {
     this.errorHidden = true;
   }
 
