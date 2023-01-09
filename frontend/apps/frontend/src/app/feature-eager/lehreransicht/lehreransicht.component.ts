0779d950cd2aa66daa445b9a5d63290fe01a6149
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2023-01-09 09:30:24 +0100

    Post Umfrage

--- frontend/apps/frontend/src/app/feature-eager/lehreransicht/lehreransicht.component.ts
+++ frontend/apps/frontend/src/app/feature-eager/lehreransicht/lehreransicht.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PollOptionReplayDto, PollReplayDto, PollsService } from '../../polls-backend';
 
 @Component({
   selector: 'app-lehreransicht',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lehreransicht.component.scss']
 })
 
-export class LehreransichtComponent implements OnInit {
+export class LehreransichtComponent {
   title = '';
   question = '';
   options: string[] = [];
@@ -16,18 +17,38 @@ export class LehreransichtComponent implements OnInit {
   timeTo = '';
   multipleChoice = false;
 
-  constructor() { }
+  constructor(private pollsService: PollsService) { }
 
-  ngOnInit(): void {
-    console.log('LehreransichtComponent.ngOnInit()');
-  }
+  // ngOnInit(): void {
+  // }
 
   createPoll(): void {
     console.log(`${this.title} ${this.question} ${this.options[0]}${this.options[1]}${this.options[2]} ${this.dateFrom} ${this.timeFrom} ${this.dateTo} ${this.timeTo} ${this.multipleChoice}`);
+    const pollOptions: PollOptionReplayDto[] = []
+    this.options.forEach(x => {
+      const option: PollOptionReplayDto = {
+        description: x
+      };
+      pollOptions.push(option);
+    });
 
-    // const pollDto: PollReplayDto = {
+    const startTime = this.dateFrom + "T" + this.timeFrom + ":00.000Z";
+    const endTime = this.dateTo + "T" + this.timeTo + ":00.000Z";
 
-    // }
-    // this.pollsService.pollsPost()
+    console.log(startTime);
+    console.log(endTime);
+
+    const pollDto: PollReplayDto = {
+      pollName: this.title,
+      pollQuestion: this.question,
+      pollOptions: pollOptions,
+      startTime: this.dateFrom + " " + this.timeFrom,
+      endTime: this.dateTo + " " + this.timeTo,
+      isMultipleChoice: this.multipleChoice,
+      // createdBy: '080912bd-fc81-4f1a-b47f-e7ab8d2add2e'
+    }
+    this.pollsService.pollsPost(pollDto).subscribe(x => {
+      console.log(JSON.stringify(x));
+    });
   }
 }
