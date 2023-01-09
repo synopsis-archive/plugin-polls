0f5e3c9ef6c06ea1b121d59addb348049bb1384a
fnadler18 <florian.nadler1@gmail.com>
2023-01-09 08:20:37 +0100

    deleted old swagger folder

--- frontend/apps/frontend/src/app/feature-eager/code-input/code-input.component.ts
+++ frontend/apps/frontend/src/app/feature-eager/code-input/code-input.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { PollsService } from '../../swagger';
 import { catchError } from 'rxjs';
+import { PollsService } from '../../polls-backend';
 
 @Component({
   selector: 'app-code-input',
